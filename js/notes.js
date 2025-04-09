61e72cb8ee0b211667087e4ba21a9b13d814d65b
Linda Bengtsson <linda-bengsson@outlook.com>
2025-04-09 19:01:20 +0200

    initial commit

---
+++ js/notes.js
@@ -0,0 +1,13 @@
+const notePad = document.getElementById("notes");
+
+// get saved notes
+const savedNotes = localStorage.getItem("notes");
+if (savedNotes) {
+  notePad.value = savedNotes;
+}
+
+// save notes to localstorage
+notePad.addEventListener("input", () => {
+  let notes = notePad.value;
+  localStorage.setItem("notes", notes);
+});
